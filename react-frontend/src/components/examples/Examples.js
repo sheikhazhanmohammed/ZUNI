909d7086cc95a5d5bf2e9acf3ec20a7b2b34e2e7
MD Rashid Hussain <mdrashid.hussain.786.01@gmail.com>
2021-11-03 21:48:16 +0530

    add demo video

--- react-frontend/src/components/examples/Examples.js
+++ react-frontend/src/components/examples/Examples.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Examples() {
   return (
     <main id='examples'>
-
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/7e7_4ckG56Y" title="Demonstration of ZUNI" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
     </main>
   )
 }
